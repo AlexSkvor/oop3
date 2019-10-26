b8cd1396105ad0560b3a46cc68a769bb9746e460
Alex Starling <megoskvor95@yandex.ru>
2019-10-26 16:45:05 +0200

    Update App.kt

--- src/main/kotlin/ru/lingstra/oop3/App.kt
+++ src/main/kotlin/ru/lingstra/oop3/App.kt
@@ -7,16 +7,28 @@ object App {
     fun main(args: Array<String>) {
         val generator = InputGenerator()
         val reader = Reader()
-        val stupid = Heuristic1()
         val answerWriter = AnswerWriter()
-        val task = listOf(10, 100, 1000, 10000, 100000/*, 1000000*/)
+        val task = listOf(10, 100, 1000, 10000, 100000, 200000, 500000)
+
         generator.generate(task)
+
+        val stupid = Heuristic1()
+        val clever = Heuristic2()
+
         task.forEach {
-            val test = reader.read(it).reversed()
+            val test = reader.read(it)
             val start = Date().time
             val answer = stupid.pack(test)
             val end = Date().time
-            answerWriter.print(answer, it, end - start)
+            answerWriter.print(answer, it, end - start, "BAD")
+        }
+
+        task.forEach {
+            val test = reader.read(it)
+            val start = Date().time
+            val answer = clever.pack(test)
+            val end = Date().time
+            answerWriter.print(answer, it, end - start, "GOOD")
         }
     }
 }
