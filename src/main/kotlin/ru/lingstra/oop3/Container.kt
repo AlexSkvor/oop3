3724654a384ed2b33d1026326ad4c1be56d8ccec
Alex Starling <megoskvor95@yandex.ru>
2019-10-26 16:46:38 +0200

    Create Container.kt

---
+++ src/main/kotlin/ru/lingstra/oop3/Container.kt
@@ -0,0 +1,24 @@
+package ru.lingstra.oop3
+
+data class Container(
+    val items: MutableList<Int>,
+    var currentSize: Int
+) {
+
+    constructor(firstItem: Int) : this(mutableListOf(firstItem), firstItem)
+
+    companion object {
+        const val MAX_SIZE: Int = 1000000
+    }
+
+    val remainingSize: Int
+        get() = MAX_SIZE - currentSize
+
+    fun add(item: Int) {
+        items.add(item)
+        currentSize += item
+        require(currentSize <= MAX_SIZE) { "Too much for this $this" }
+    }
+
+    fun canAdd(item: Int) = currentSize + item <= MAX_SIZE
+}
