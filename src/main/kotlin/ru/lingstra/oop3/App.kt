fdd85c7fac4e33d2aa0b8dec7fcee0d5451cd66e
AlexSkvor <megoskvor95@yandex.ru>
2019-10-28 00:16:17 +0200

    finished build problem

--- src/main/kotlin/ru/lingstra/oop3/App.kt
+++ src/main/kotlin/ru/lingstra/oop3/App.kt
@@ -2,35 +2,32 @@ package ru.lingstra.oop3
 
 import java.util.*
 
-object App {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val generator = InputGenerator()
-        val reader = Reader()
-        val answerWriter = AnswerWriter()
-        val task = listOf(10, 100, 1000, 10000, 100000, 200000, 500000, 1000000)
+fun main(args: Array<String>) {
+    val generator = InputGenerator()
+    val reader = Reader()
+    val answerWriter = AnswerWriter()
+    val task = listOf(10, 100, 1000, 10000, 100000, 200000, 500000, 1000000)
 
-        generator.generate(task)
+    generator.generate(task)
 
-        val stupid = Heuristic1()
-        val clever = Heuristic2()
+    val stupid = Heuristic1()
+    val clever = Heuristic2()
 
-        task.forEach {
-            if (it < 200001) {
-                val test = reader.read(it)
-                val start = Date().time
-                val answer = stupid.pack(test)
-                val end = Date().time
-                answerWriter.print(answer, it, end - start, "BAD")
-            }
-        }
-
-        task.forEach {
+    task.forEach {
+        if (it < 200001) {
             val test = reader.read(it)
             val start = Date().time
-            val answer = clever.pack(test)
+            val answer = stupid.pack(test)
             val end = Date().time
-            answerWriter.print(answer, it, end - start, "GOOD")
+            answerWriter.print(answer, it, end - start, "BAD")
         }
+    }
+
+    task.forEach {
+        val test = reader.read(it)
+        val start = Date().time
+        val answer = clever.pack(test)
+        val end = Date().time
+        answerWriter.print(answer, it, end - start, "GOOD")
     }
 }
